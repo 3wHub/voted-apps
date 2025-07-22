d6d662e50dd52087b5126c2f38f028c16ca2cc26
Arbyusman <acingman13@gmail.com>
2025-07-22 22:33:46 +0800

    chore: generate declarations file

--- src/declarations/backend/backend.did.d.ts
+++ src/declarations/backend/backend.did.d.ts
@@ -3,143 +3,27 @@ import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
 export interface _SERVICE {
-  'login': ActorMethod<[], string>,
-  'logout': ActorMethod<[], string>,
-  'whoAmI': ActorMethod<[], string>,
-  'castVote' : ActorMethod<
-    [string, string, string],
-    [] | [
-      {
-        'id' : string,
-        'updated_at' : string,
-        'question' : string,
-        'totalVotes' : number,
-        'tags' : Array<string>,
-        'created_at' : string,
-        'options' : Array<
-          { 'id' : string, 'votes' : number, 'label' : string }
-        >,
-      }
-    ]
-  >,
+  'castVote' : ActorMethod<[string, string], undefined>,
   'createPoll' : ActorMethod<
     [
       string,
       Array<{ 'id' : string, 'votes' : number, 'label' : string }>,
       Array<string>,
-      string,
     ],
-    {
-      'id' : string,
-      'updated_at' : string,
-      'question' : string,
-      'totalVotes' : number,
-      'tags' : Array<string>,
-      'created_at' : string,
-      'created_by' : string,
-      'options' : Array<{ 'id' : string, 'votes' : number, 'label' : string }>,
-    }
+    undefined
   >,
-  'deletePoll' : ActorMethod<
-    [string],
-    [] | [
-      {
-        'id' : string,
-        'updated_at' : string,
-        'question' : string,
-        'totalVotes' : number,
-        'tags' : Array<string>,
-        'created_at' : string,
-        'options' : Array<
-          { 'id' : string, 'votes' : number, 'label' : string }
-        >,
-      }
-    ]
-  >,
-  'getAllPolls' : ActorMethod<
-    [],
-    Array<
-      {
-        'id' : string,
-        'updated_at' : string,
-        'question' : string,
-        'totalVotes' : number,
-        'tags' : Array<string>,
-        'created_at' : string,
-        'options' : Array<
-          { 'id' : string, 'votes' : number, 'label' : string }
-        >,
-      }
-    >
-  >,
-  'getPoll' : ActorMethod<
-    [string],
-    [] | [
-      {
-        'id' : string,
-        'updated_at' : string,
-        'question' : string,
-        'totalVotes' : number,
-        'tags' : Array<string>,
-        'created_at' : string,
-        'created_by' : string,
-        'options' : Array<
-          { 'id' : string, 'votes' : number, 'label' : string }
-        >,
-      }
-    ]
-  >,
-  'getPollOptions' : ActorMethod<
-    [string],
-    [] | [Array<{ 'id' : string, 'votes' : number, 'label' : string }>]
-  >,
-  'getPollsByAgent' : ActorMethod<
-    [string],
-    Array<
-      {
-        'id' : string,
-        'updated_at' : string,
-        'question' : string,
-        'totalVotes' : number,
-        'tags' : Array<string>,
-        'created_at' : string,
-        'created_by' : string,
-        'options' : Array<
-          { 'id' : string, 'votes' : number, 'label' : string }
-        >,
-      }
-    >
-  >,
-  'getPollsByTag' : ActorMethod<
-    [string],
-    Array<
-      {
-        'id' : string,
-        'updated_at' : string,
-        'question' : string,
-        'totalVotes' : number,
-        'tags' : Array<string>,
-        'created_at' : string,
-        'options' : Array<
-          { 'id' : string, 'votes' : number, 'label' : string }
-        >,
-      }
-    >
-  >,
-  'getVoteCountForOption' : ActorMethod<[string, string], [] | [number]>,
-  'getVotesForPoll' : ActorMethod<
-    [string],
-    Array<
-      {
-        'id' : string,
-        'optionId' : string,
-        'votedAt' : string,
-        'voterId' : string,
-        'pollId' : string,
-      }
-    >
-  >,
-  'hasVoted' : ActorMethod<[string, string], boolean>,
+  'getAllPolls' : ActorMethod<[], undefined>,
+  'getMyPolls' : ActorMethod<[], undefined>,
+  'getPoll' : ActorMethod<[string], undefined>,
+  'getPollOptions' : ActorMethod<[string], undefined>,
+  'getPollsByAgent' : ActorMethod<[], undefined>,
+  'getPollsByTag' : ActorMethod<[string], undefined>,
+  'getVoteCountForOption' : ActorMethod<[string, string], undefined>,
+  'getVotesForPoll' : ActorMethod<[string], undefined>,
+  'hasVoted' : ActorMethod<[string], undefined>,
+  'login' : ActorMethod<[], undefined>,
+  'logout' : ActorMethod<[], undefined>,
+  'whoAmI' : ActorMethod<[], undefined>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- src/declarations/backend/backend.did.js
+++ src/declarations/backend/backend.did.js
@@ -1,31 +1,6 @@
 export const idlFactory = ({ IDL }) => {
   return IDL.Service({
-    'login' : IDL.Func([], [], []),
-    'logout' : IDL.Func([], [], []),
-    'whoAmI' : IDL.Func([], [], ['query']),
-    'castVote' : IDL.Func(
-        [IDL.Text, IDL.Text, IDL.Text],
-        [
-          IDL.Opt(
-            IDL.Record({
-              'id' : IDL.Text,
-              'updated_at' : IDL.Text,
-              'question' : IDL.Text,
-              'totalVotes' : IDL.Nat32,
-              'tags' : IDL.Vec(IDL.Text),
-              'created_at' : IDL.Text,
-              'options' : IDL.Vec(
-                IDL.Record({
-                  'id' : IDL.Text,
-                  'votes' : IDL.Nat32,
-                  'label' : IDL.Text,
-                })
-              ),
-            })
-          ),
-        ],
-        [],
-      ),
+    'castVote' : IDL.Func([IDL.Text, IDL.Text], [], []),
     'createPoll' : IDL.Func(
         [
           IDL.Text,
@@ -37,183 +12,22 @@ export const idlFactory = ({ IDL }) => {
             })
           ),
           IDL.Vec(IDL.Text),
-          IDL.Text,
-        ],
-        [
-          IDL.Record({
-            'id' : IDL.Text,
-            'updated_at' : IDL.Text,
-            'question' : IDL.Text,
-            'totalVotes' : IDL.Nat32,
-            'tags' : IDL.Vec(IDL.Text),
-            'created_at' : IDL.Text,
-            'created_by' : IDL.Text,
-            'options' : IDL.Vec(
-              IDL.Record({
-                'id' : IDL.Text,
-                'votes' : IDL.Nat32,
-                'label' : IDL.Text,
-              })
-            ),
-          }),
         ],
         [],
-      ),
-    'deletePoll' : IDL.Func(
-        [IDL.Text],
-        [
-          IDL.Opt(
-            IDL.Record({
-              'id' : IDL.Text,
-              'updated_at' : IDL.Text,
-              'question' : IDL.Text,
-              'totalVotes' : IDL.Nat32,
-              'tags' : IDL.Vec(IDL.Text),
-              'created_at' : IDL.Text,
-              'options' : IDL.Vec(
-                IDL.Record({
-                  'id' : IDL.Text,
-                  'votes' : IDL.Nat32,
-                  'label' : IDL.Text,
-                })
-              ),
-            })
-          ),
-        ],
         [],
       ),
-    'getAllPolls' : IDL.Func(
-        [],
-        [
-          IDL.Vec(
-            IDL.Record({
-              'id' : IDL.Text,
-              'updated_at' : IDL.Text,
-              'question' : IDL.Text,
-              'totalVotes' : IDL.Nat32,
-              'tags' : IDL.Vec(IDL.Text),
-              'created_at' : IDL.Text,
-              'options' : IDL.Vec(
-                IDL.Record({
-                  'id' : IDL.Text,
-                  'votes' : IDL.Nat32,
-                  'label' : IDL.Text,
-                })
-              ),
-            })
-          ),
-        ],
-        ['query'],
-      ),
-    'getPoll' : IDL.Func(
-        [IDL.Text],
-        [
-          IDL.Opt(
-            IDL.Record({
-              'id' : IDL.Text,
-              'updated_at' : IDL.Text,
-              'question' : IDL.Text,
-              'totalVotes' : IDL.Nat32,
-              'tags' : IDL.Vec(IDL.Text),
-              'created_at' : IDL.Text,
-              'created_by' : IDL.Text,
-              'options' : IDL.Vec(
-                IDL.Record({
-                  'id' : IDL.Text,
-                  'votes' : IDL.Nat32,
-                  'label' : IDL.Text,
-                })
-              ),
-            })
-          ),
-        ],
-        ['query'],
-      ),
-    'getPollOptions' : IDL.Func(
-        [IDL.Text],
-        [
-          IDL.Opt(
-            IDL.Vec(
-              IDL.Record({
-                'id' : IDL.Text,
-                'votes' : IDL.Nat32,
-                'label' : IDL.Text,
-              })
-            )
-          ),
-        ],
-        ['query'],
-      ),
-    'getPollsByAgent' : IDL.Func(
-        [IDL.Text],
-        [
-          IDL.Vec(
-            IDL.Record({
-              'id' : IDL.Text,
-              'updated_at' : IDL.Text,
-              'question' : IDL.Text,
-              'totalVotes' : IDL.Nat32,
-              'tags' : IDL.Vec(IDL.Text),
-              'created_at' : IDL.Text,
-              'created_by' : IDL.Text,
-              'options' : IDL.Vec(
-                IDL.Record({
-                  'id' : IDL.Text,
-                  'votes' : IDL.Nat32,
-                  'label' : IDL.Text,
-                })
-              ),
-            })
-          ),
-        ],
-        ['query'],
-      ),
-    'getPollsByTag' : IDL.Func(
-        [IDL.Text],
-        [
-          IDL.Vec(
-            IDL.Record({
-              'id' : IDL.Text,
-              'updated_at' : IDL.Text,
-              'question' : IDL.Text,
-              'totalVotes' : IDL.Nat32,
-              'tags' : IDL.Vec(IDL.Text),
-              'created_at' : IDL.Text,
-              'options' : IDL.Vec(
-                IDL.Record({
-                  'id' : IDL.Text,
-                  'votes' : IDL.Nat32,
-                  'label' : IDL.Text,
-                })
-              ),
-            })
-          ),
-        ],
-        ['query'],
-      ),
-    'getVoteCountForOption' : IDL.Func(
-        [IDL.Text, IDL.Text],
-        [IDL.Opt(IDL.Nat32)],
-        ['query'],
-      ),
-    'getVotesForPoll' : IDL.Func(
-        [IDL.Text],
-        [
-          IDL.Vec(
-            IDL.Record({
-              'id' : IDL.Text,
-              'optionId' : IDL.Text,
-              'votedAt' : IDL.Text,
-              'voterId' : IDL.Text,
-              'pollId' : IDL.Text,
-            })
-          ),
-        ],
-        ['query'],
-      ),
-    'hasVoted' : IDL.Func([IDL.Text, IDL.Text], [IDL.Bool], ['query']),
+    'getAllPolls' : IDL.Func([], [], ['query']),
+    'getMyPolls' : IDL.Func([], [], ['query']),
+    'getPoll' : IDL.Func([IDL.Text], [], ['query']),
+    'getPollOptions' : IDL.Func([IDL.Text], [], ['query']),
+    'getPollsByAgent' : IDL.Func([], [], ['query']),
+    'getPollsByTag' : IDL.Func([IDL.Text], [], ['query']),
+    'getVoteCountForOption' : IDL.Func([IDL.Text, IDL.Text], [], ['query']),
+    'getVotesForPoll' : IDL.Func([IDL.Text], [], ['query']),
+    'hasVoted' : IDL.Func([IDL.Text], [], ['query']),
+    'login' : IDL.Func([], [], []),
+    'logout' : IDL.Func([], [], []),
+    'whoAmI' : IDL.Func([], [], ['query']),
   });
 };
-export const init = ({ IDL }) => {
-  return [];
-};
+export const init = ({ IDL }) => { return []; };
