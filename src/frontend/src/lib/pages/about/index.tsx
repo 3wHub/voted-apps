6e14c665c5bf38625051727d942855ecef0dafa5
arbyusman <acingman13@gmail.com>
2025-07-25 09:57:39 +0800

    fix: duplicate button link

--- src/frontend/src/lib/pages/about/index.tsx
+++ src/frontend/src/lib/pages/about/index.tsx
@@ -100,15 +100,6 @@ export default function About() {
             </svg>
           </button>
         )}
-        <Link
-          to="/votes/create"
-          className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 text-base"
-        >
-          Create Your First Vote
-          <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-          </svg>
-        </Link>
       </section>
     </Container>
   );
